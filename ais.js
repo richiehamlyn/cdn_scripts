2dc5d2ecd5bc705aeed90f04478fd7a7502ef385
Richie Hamlyn <richardhamlyn@gmail.com>
2022-11-17 12:39:00 +0000

    Update ais.js

--- ais.js
+++ ais.js
@@ -2,8 +2,7 @@
     a[b].q=[];c=d.createElement(e);c.async=1;c.src="//tdn.r42tag.com/lib/"+f+".js";
     s=d.getElementsByTagName(e)[0];s.parentNode.insertBefore(c,s);})
     (window,document,"script","_st", "1321-v1");
-    _st('setCookieDomain', 'www.leedsunited.com/');
+    _st('setCookieDomain', 'www.gap.co.uk');
     _st('setPageStructure', 'home');
-    _st('addLocalTagProperties',  {'email':'visitorEmail','name':'visitorName','subscribed':'true'});
     _st('loadTags');
     console.log('Relay42 basescript running');
