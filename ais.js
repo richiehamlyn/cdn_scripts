05ace3ce5675aa00d78912fc5b996a0909ffed1d
Richie Hamlyn <richardhamlyn@gmail.com>
2022-11-17 12:28:29 +0000

    Update ais.js

--- ais.js
+++ ais.js
@@ -2,7 +2,7 @@
     a[b].q=[];c=d.createElement(e);c.async=1;c.src="//tdn.r42tag.com/lib/"+f+".js";
     s=d.getElementsByTagName(e)[0];s.parentNode.insertBefore(c,s);})
     (window,document,"script","_st", "1321-v1");
-    _st('setCookieDomain', 'www.ais.th/en');
+    _st('setCookieDomain', 'www.leedsunited.com/');
     _st('setPageStructure', 'home');
     _st('addLocalTagProperties',  {'email':'visitorEmail','name':'visitorName','subscribed':'true'});
     _st('loadTags');
