a1c61ab5984c19ebcfcec636da07c9c1a3b5ab6e
Richie Hamlyn <richardhamlyn@gmail.com>
2022-11-18 09:44:51 +0000

    Update ais.js

--- ais.js
+++ ais.js
@@ -1,7 +1,7 @@
 (function(a,d,e,b,f,c,s){a[b]=a[b]||function(){a[b].q.push(arguments);};
     a[b].q=[];c=d.createElement(e);c.async=1;c.src="//tdn.r42tag.com/lib/"+f+".js";
     s=d.getElementsByTagName(e)[0];s.parentNode.insertBefore(c,s);})
-    (window,document,"script","_st", "1321-v1");
+    (window,document,"script","_st", "1100-v1");
     _st('setCookieDomain', 'www.gap.co.uk');
     _st('setPageStructure', 'home');
     _st('loadTags');
