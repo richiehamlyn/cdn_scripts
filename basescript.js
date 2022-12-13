3afc52459fe87e1ce84d3a49ba3f7100bed79433
Richie Hamlyn <richardhamlyn@gmail.com>
2022-12-13 11:31:16 +0000

    Create basescript.js
    New for Koton

---
+++ basescript.js
@@ -0,0 +1,8 @@
+(function(a,d,e,b,f,c,s){a[b]=a[b]||function(){a[b].q.push(arguments);};
+    a[b].q=[];c=d.createElement(e);c.async=1;c.src="//tdn.r42tag.com/lib/"+f+".js";
+    s=d.getElementsByTagName(e)[0];s.parentNode.insertBefore(c,s);})
+    (window,document,"script","_st", "1100-v1");
+    _st('setCookieDomain', 'www.koton.com');
+    _st('setPageStructure', 'home');
+    _st('loadTags');
+    console.log('Relay42 basescript running for Koton account from CDN');
