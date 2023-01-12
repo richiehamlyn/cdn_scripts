404711cb18db63f792739acb394835b84ab37adc
Richie Hamlyn <richardhamlyn@gmail.com>
2023-01-12 10:13:59 +0000

    Update basescript.js

--- basescript.js
+++ basescript.js
@@ -2,7 +2,7 @@
     a[b].q=[];c=d.createElement(e);c.async=1;c.src="//tdn.r42tag.com/lib/"+f+".js";
     s=d.getElementsByTagName(e)[0];s.parentNode.insertBefore(c,s);})
     (window,document,"script","_st", "1100-v1");
-    _st('setCookieDomain', 'www.koton.com');
+    _st('setCookieDomain', 'world.expeditions.com');
     _st('setPageStructure', 'home');
     _st('loadTags');
-    console.log('Relay42 basescript running for Koton account from CDN');
+    console.log('Relay42 basescript running for Lindbald account from CDN');
