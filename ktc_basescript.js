7d1c31ead43377295e1ca5f3fa0c41dce9da6866
Richie Hamlyn <richardhamlyn@gmail.com>
2023-02-07 14:46:02 +0000

    Update ktc_basescript.js
    Update ktc_basescript.js

--- ktc_basescript.js
+++ ktc_basescript.js
@@ -2,7 +2,7 @@
     a[b].q=[];c=d.createElement(e);c.async=1;c.src="//tdn.r42tag.com/lib/"+f+".js";
     s=d.getElementsByTagName(e)[0];s.parentNode.insertBefore(c,s);})
     (window,document,"script","_st", "1100-v1");
-    _st('setCookieDomain', 'www.ktc.co.th/');
+    _st('setCookieDomain', 'www.ktc.co.th');
     _st('setPageStructure', 'home');
     _st('loadTags');
     console.log('Git Hub: Relay42 basescript running for KTC account from CDN');
