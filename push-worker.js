08c106d3abb05eae418feb80e3dd7921790c5144
Richie Hamlyn <richardhamlyn@gmail.com>
2024-04-12 11:34:28 +0100

    Add files via upload

---
+++ push-worker.js
@@ -0,0 +1,11 @@
+// 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1712917301.7761295
+importScripts('https://aswpsdkeu.com/notify/v1/ua-sdk.min.js')
+uaSetup.worker(self, {
+  
+  defaultIcon: 'https://www.relay42.com',
+  defaultTitle: 'r42WebChannel',
+  defaultActionURL: 'https://hookb.in/xYL33P2QbRI7zzYJ3r1D',
+  appKey: 'IhHRSOjXTae4YHs0ZN815A',
+  token: 'MTpJaEhSU09qWFRhZTRZSHMwWk44MTVBOjByemtqWG1VcDJueGV0VVpNOWZXdW1DV090UVM5YzZ0UEhIZlMyRUwyZGc',
+  vapidPublicKey: 'BALrKQANxn6twsijYEDmLuxB8uNxZHQmWV6m6T3qv7RBkl8aOU-Ty7q3ZKvwRMW1csiNENbp9GhcF1zXJBJ2WjQ='
+})
